1dae1bb25fe4df8d4a36fb3eb3ec7bab89471472
OrkasST <egor31@outlook.com>
2024-02-14 21:30:34 +0300

    Started creating GameObject class 21:30_14.02.2024

--- scripts/objects/GameObject.js
+++ scripts/objects/GameObject.js
@@ -1,3 +1,20 @@
 export class GameObject {
-  constructor() {}
+  constructor({
+    x = 0,
+    y = 0,
+    width = 0,
+    height = 0,
+    isSpheric = false,
+    radius = 0,
+    isStatic = true,
+    isCollidable = true,
+  }) {
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+
+    this.isStatic = isStatic;
+    this.isCollidable = isCollidable;
+  }
 }
