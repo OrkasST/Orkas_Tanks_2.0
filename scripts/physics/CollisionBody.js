5ece2a6eadc7724397f23cb6a609ad0f5961bac2
OrkasST <egor31@outlook.com>
2024-02-16 10:25:31 +0300

    completed extended collisions for rect/rect and circle/circle :: 10:25-16.02.2024

--- scripts/physics/CollisionBody.js
+++ scripts/physics/CollisionBody.js
@@ -16,7 +16,7 @@ export class CollisionBody {
     */
   }
 
-  checkCollision(body, extended = false) {
+  checkCollision(body) {
     console.log("body: ", body);
     //body object must have the same structure as this.bodies
     for (let i = 0; i < body.length; i++) {
@@ -31,7 +31,7 @@ export class CollisionBody {
             body[i].y2 >= this.bodies[j].y1
           ) {
             //debugger;
-            return extended ? [] : true;
+            return true;
           } else continue; //bodies are rect type but no collision
         }
 
@@ -45,7 +45,7 @@ export class CollisionBody {
             body[i].r + this.bodies[j].r
           ) {
             //debugger;
-            return extended ? [] : true;
+            return true;
           } else continue; //bodies are circle type but no collision
         }
 
@@ -78,7 +78,7 @@ export class CollisionBody {
             ) <= this.bodies[j].r
           ) {
             //debugger;
-            return extended ? [] : true;
+            return true;
           } else continue; //colliding body is rect, this is circle no collision
         }
 
@@ -111,7 +111,139 @@ export class CollisionBody {
             ) <= body[i].r
           ) {
             //debugger;
-            return extended ? [] : true;
+            return true;
+          } else continue; //colliding body is circle, this is rect no collision
+        }
+      }
+    }
+    return false;
+  }
+
+  extendedCollisionCheck(body) {
+    console.log("body: ", body);
+    //body object must have the same structure as this.bodies
+
+    let collidingDirections = [0, 0, 0, 0];
+    //[0]: 1 (left), 0 (none)
+    //[1]: 1 (right), 0 (none)
+    //[2]: 1 (up), 0 (none)
+    //[3]: 1 (down), 0 (none)
+
+    for (let i = 0; i < body.length; i++) {
+      for (let j = 0; j < this.bodies.length; j++) {
+        if (body[i].type === 0 && this.bodies[j].type === 0) {
+          console.log("Rects");
+          //debugger;
+          if (
+            this.bodies[j].x1 <= body[i].x2 &&
+            this.bodies[j].x2 > body[i].x2 &&
+            this.bodies[j].y2 > body[i].y1 &&
+            this.bodies[j].y1 < body[i].y2
+          ) {
+            //debugger;
+            collidingDirections[0] = 1; //left
+          } else if (
+            this.bodies[j].x2 >= body[i].x1 &&
+            this.bodies[j].x1 < body[i].x1 &&
+            this.bodies[j].y2 > body[i].y1 &&
+            this.bodies[j].y1 < body[i].y2
+          ) {
+            //debugger;
+            collidingDirections[1] = 1; //right
+          } else if (
+            this.bodies[j].y1 <= body[i].y2 &&
+            this.bodies[j].y2 > body[i].y2 &&
+            this.bodies[j].x2 > body[i].x1 &&
+            this.bodies[j].x1 < body[i].x2
+          ) {
+            //debugger;
+            collidingDirections[2] = 1; //up
+          } else if (
+            this.bodies[j].y2 >= body[i].y1 &&
+            this.bodies[j].y1 < body[i].y1 &&
+            this.bodies[j].x2 > body[i].x1 &&
+            this.bodies[j].x1 < body[i].x2
+          ) {
+            //debugger;
+            collidingDirections[3] = 1; //down
+          } else continue; //bodies are rect type but no collision
+        } else if (body[i].type === 1 && this.bodies[j].type === 1) {
+          //debugger;
+          if (
+            Math.sqrt(
+              (body[i].x - this.bodies[j].x) * (body[i].x - this.bodies[j].x) +
+                (body[i].y - this.bodies[j].y) * (body[i].y - this.bodies[j].y)
+            ) <=
+            body[i].r + this.bodies[j].r
+          ) {
+            //debugger;
+            if (this.bodies[j].y < body[i].y) collidingDirections[3] = 1; //down
+            else if (this.bodies[j].y > body[i].y) collidingDirections[2] = 1; //up
+            if (this.bodies[j].x < body[i].x)
+              collidingDirections[1] = 1; //right
+            else if (this.bodies[j].x < body[i].x) collidingDirections[0] = 1; //left
+          } else continue; //bodies are circle type but no collision
+        } else if (body[i].type === 0 && this.bodies[j].type === 1) {
+          //debugger;
+          if (
+            Math.sqrt(
+              (body[i].x1 - this.bodies[j].x) *
+                (body[i].x1 - this.bodies[j].x) +
+                (body[i].y1 - this.bodies[j].y) *
+                  (body[i].y1 - this.bodies[j].y)
+            ) <= this.bodies[j].r || //checking if distance from all rectangle points to center of circle is smaller than radius
+            Math.sqrt(
+              (body[i].x1 - this.bodies[j].x) *
+                (body[i].x1 - this.bodies[j].x) +
+                (body[i].y2 - this.bodies[j].y) *
+                  (body[i].y2 - this.bodies[j].y)
+            ) <= this.bodies[j].r ||
+            Math.sqrt(
+              (body[i].x2 - this.bodies[j].x) *
+                (body[i].x2 - this.bodies[j].x) +
+                (body[i].y1 - this.bodies[j].y) *
+                  (body[i].y1 - this.bodies[j].y)
+            ) <= this.bodies[j].r ||
+            Math.sqrt(
+              (body[i].x2 - this.bodies[j].x) *
+                (body[i].x2 - this.bodies[j].x) +
+                (body[i].y2 - this.bodies[j].y) *
+                  (body[i].y2 - this.bodies[j].y)
+            ) <= this.bodies[j].r
+          ) {
+            //debugger;
+            return true;
+          } else continue; //colliding body is rect, this is circle no collision
+        } else if (body[i].type === 1 && this.bodies[j].type === 0) {
+          //debugger;
+          if (
+            Math.sqrt(
+              (this.bodies[j].x1 - body[i].x) *
+                (this.bodies[j].x1 - body[i].x) +
+                (this.bodies[j].y1 - body[i].y) *
+                  (this.bodies[j].y1 - body[i].y)
+            ) <= body[i].r || //checking if distance from all rectangle points to center of circle is smaller than radius
+            Math.sqrt(
+              (this.bodies[j].x1 - body[i].x) *
+                (this.bodies[j].x1 - body[i].x) +
+                (this.bodies[j].y2 - body[i].y) *
+                  (this.bodies[j].y2 - body[i].y)
+            ) <= body[i].r ||
+            Math.sqrt(
+              (this.bodies[j].x2 - body[i].x) *
+                (this.bodies[j].x2 - body[i].x) +
+                (this.bodies[j].y1 - body[i].y) *
+                  (this.bodies[j].y1 - body[i].y)
+            ) <= body[i].r ||
+            Math.sqrt(
+              (this.bodies[j].x2 - body[i].x) *
+                (this.bodies[j].x2 - body[i].x) +
+                (this.bodies[j].y2 - body[i].y) *
+                  (this.bodies[j].y2 - body[i].y)
+            ) <= body[i].r
+          ) {
+            //debugger;
+            return true;
           } else continue; //colliding body is circle, this is rect no collision
         }
       }
